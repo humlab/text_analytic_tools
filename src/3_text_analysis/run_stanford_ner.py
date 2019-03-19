b06bbb6499ea09db02c048c44cb89333bd8cfb07
roger-mahler <roger.mahler@hotmail.com>
2019-03-19 11:35:37 +0100

    NER stuff

--- src/3_text_analysis/run_stanford_ner.py
+++ src/3_text_analysis/run_stanford_ner.py
@@ -6,6 +6,7 @@ import text_corpus
 import domain_logic_vatican as domain_logic
 import nltk
 import pandas as pd
+import zipfile
 
 sys.path = list(set(['.', '..']) - set(sys.path)) + sys.path
 
@@ -25,7 +26,7 @@ def merge_entities(entities):
     merged = entities[:1]
     for doc_id, i_n, w_n, t_n in entities[1:]:
         doc_id_p, i_p, w_p, t_p = merged[-1]
-        if doc_id == doc_id_p and i_n == i_p + 1 and t_n == t_p:
+        if i_n == i_p + 1 and t_n == t_p:
             merged[-1] = (doc_id, i_n, '_'.join([w_p, w_n]), t_p)
         else:
             merged.append((doc_id, i_n, w_n, t_n))
@@ -35,13 +36,30 @@ def recognize_named_entities(tagger, doc_id, text, excludes=None):
     sentences = nltk.sent_tokenize(text)
     start_index = 0
     excludes = excludes or []
+    merged_ents = []
     for sentence in sentences:
         tokens = nltk.word_tokenize(sentence)
         ents = [ (doc_id, start_index + index, word, ent_type) for index, (word, ent_type) in enumerate(tagger.tag(tokens)) if ent_type not in excludes ]
         start_index += len(sentence)
-    merged_ents = merge_entities(ents)
+        merged_ents = merge_entities(ents)
     return merged_ents
+
+#
+#     if not os.path.isfile(targetfile) or overwrite:
+#         if os.path.isfile(targetfile):
+#             os.remove(targetfile)
+#         with zipfile.ZipFile(targetfile, "w", compression=zipfile.ZIP_DEFLATED) as z:
+#             pass
+#     else:
         
+#         with zipfile.ZipFile(targetfile, "r") as z:
+#             zip_filenames = z.namelist()
+            
+#         pdf_filepaths = list(set(pdf_filepaths) - set(zip_filenames))
+    
+#     with zipfile.ZipFile(targetfile, "a", compression=zipfile.ZIP_DEFLATED) as z:
+#         z.writestr(text_filename, pdf_text)
+            
 def compute_stanford_ner(source_file, service_url=STANFORD_CORE_NLP_URL, excludes=None):
     
     excludes = excludes or ('O', )
@@ -55,23 +73,24 @@ def compute_stanford_ner(source_file, service_url=STANFORD_CORE_NLP_URL, exclude
     stream = domain_logic.get_document_stream(reader, 'en', document_index=document_index)
     
     i = 0
+    ner_data = []
     for filename, text, metadata in stream:
-        document_id = document_index.loc[document_index.filename == filename].document_id[0]
-        ner_data = recognize_named_entities(tagger, document_id, text, excludes)
+        document_id = document_index.loc[document_index.filename == filename, 'document_id'].values[0]
+        ner = recognize_named_entities(tagger, document_id, text, excludes)
+        ner_data.extend(ner)
         i += 1
-        if i % 10:
+        if i % 10 == 0:
             logger.info('Processed {} files...'.format(i))
-            break
+            #break
     return ner_data
 
 def compute_and_store_stanford_ner(source_file):
     
     ner_data = compute_stanford_ner(source_file=source_file)
-
     df = pd.DataFrame(ner_data, columns=['doc_id', 'pos', 'entity', 'ent_type'])
     df.index.name = 'id'
 
-    store_name = '{}_{}.txt'.format(
+    store_name = 'ner_{}_{}.txt'.format(
         os.path.splitext(os.path.split(source_file)[1])[0],
         time.strftime("%Y%m%d%H%M%S", time.localtime())
     )
@@ -115,6 +134,6 @@ def display_stanford_ner_gui(data_folder):
 
 #data_folder = '../../data'
 #display_stanford_ner_gui(data_folder)
-for source_file in [ '../../data/benedict-xvi_curated_20190326.txt.zip', '../../data/francis_curated_20190326.txt.zip' ]
+for source_file in [ '../../data/benedict-xvi_curated_20190326.txt.zip', '../../data/francis_curated_20190326.txt.zip' ]:
     compute_and_store_stanford_ner(source_file)
     
