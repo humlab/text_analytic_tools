3e2c086376c1689d2e48fca1e2336e085014ecd7
Roger Mähler <roger.mahler@hotmail.com>
2019-06-28 14:51:05 +0200

    Fixed name changes in latest textacy version

--- src/3_text_analysis/textacy_corpus_gui.py
+++ src/3_text_analysis/textacy_corpus_gui.py
@@ -28,24 +28,24 @@ def generate_textacy_corpus(
     disabled_pipes=None,
     tick=utility.noop
 ):
-    
+
     for key in container.__dict__:
         container.__dict__[key] = None
-        
+
     nlp_args = { 'disable': disabled_pipes or [] }
-    
+
     store_format = 'binary' if binary_format else 'pickle'
     store_extension = 'bin' if binary_format else 'pkl'
     store_compression = 'bz2' if use_compression else ''
-    
+
     container.source_path = source_path
     container.language = language
     container.textacy_corpus = None
     container.prepped_source_path = utility.path_add_suffix(source_path, '_preprocessed')
-    
+
     if not os.path.isfile(container.prepped_source_path):
         textacy_utility.preprocess_text(container.source_path, container.prepped_source_path, tick=tick)
-        
+
     container.textacy_corpus_path = textacy_utility.generate_corpus_filename(
         container.prepped_source_path,
         container.language,
@@ -53,20 +53,20 @@ def generate_textacy_corpus(
         extension=store_extension,
         compression=store_compression
     )
-    
+
     container.nlp = textacy_utility.setup_nlp_language_model(container.language, **nlp_args)
-    
+
     if overwrite or not os.path.isfile(container.textacy_corpus_path):
-        
+
         logger.info('Working: Computing new corpus ' + container.textacy_corpus_path + '...')
-        
+
         #FIXME VARYING ASPECTS:
         reader = text_corpus.CompressedFileReader(container.prepped_source_path)
 
         stream = domain_logic.get_document_stream(reader, container.language, document_index=document_index)
-        
+
         logger.info('Working: Stream created...')
-        
+
         if False:
             tick(0, len(reader.filenames))
 
@@ -79,43 +79,43 @@ def generate_textacy_corpus(
         else:
             textacy_utility.create_textacy_corpus_streamed(stream, container.nlp, container.textacy_corpus_path, format='binary', tick=utility.noop)
             container.textacy_corpus = textacy_utility.load_corpus(container.textacy_corpus_path, container.nlp, format='binary')
-            
+
     else:
         logger.info('Working: Loading corpus ' + container.textacy_corpus_path + '...')
         tick(1, 2)
-        
+
         logger.info('...reading corpus (this might take several minutes)...')
         container.textacy_corpus = textacy_utility.load_corpus(container.textacy_corpus_path, container.nlp, format=store_format)
-        
+
     if merge_entities:
         logger.info('Working: Merging named entities...')
         try:
             for doc in container.textacy_corpus:
-                named_entities = textacy.extract.named_entities(doc)
+                named_entities = textacy.extract.entities(doc)
                 textacy.spacier.utils.merge_spans(named_entities, doc.spacy_doc)
         except Exception as ex:
             logger.error(ex)
             logger.info('NER merge failed')
     else:
         logger.info('Named entities not merged')
-                
+
     tick(0)
     logger.info('Done!')
-    
+
 def display_corpus_load_gui(data_folder, document_index=None, container=None, compute_ner=False, domain_logic=None):
-    
+
     lw = lambda w: widgets.Layout(width=w)
-    
+
     language_options = { config.LANGUAGE_MAP[k].title(): k for k in config.LANGUAGE_MAP.keys() }
-    
+
     corpus_files = sorted(glob.glob(os.path.join(data_folder, '*.txt.zip')))
-    
+
     gui = types.SimpleNamespace(
-        
+
         progress=widgets.IntProgress(value=0, min=0, max=5, step=1, description='', layout=lw('90%')),
         output=widgets.Output(layout={'border': '1px solid black'}),
         source_path=widgets_config.dropdown(description='Corpus', options=corpus_files, value=corpus_files[-1], layout=lw('300px')),
-        
+
         language=widgets_config.dropdown(description='Language', options=language_options, value='en', layout=lw('180px')),
 
         merge_entities=widgets_config.toggle('Merge NER', compute_ner, icon='', layout=lw('100px')),
@@ -123,14 +123,14 @@ def display_corpus_load_gui(data_folder, document_index=None, container=None, co
         binary_format=widgets_config.toggle('Store as binary', True, disabled=True, icon='', layout=lw('130px')),
         use_compression=widgets_config.toggle('Store compressed', True, disabled=False, icon='', layout=lw('130px')),
         overwrite=widgets_config.toggle('Force if exists', False, icon='', layout=lw('130px'), tooltip="Force generation of new corpus (even if exists)"),
-        
+
         compute_pos=widgets_config.toggle('PoS', True, icon='', layout=lw('100px'), disabled=True, tooltip="Enable Part-of-Speech tagging"),
         compute_ner=widgets_config.toggle('NER', compute_ner, icon='', layout=lw('100px'), disabled=False, tooltip="Enable named entity recognition"),
         compute_dep=widgets_config.toggle('DEP', False, icon='', layout=lw('100px'), disabled=True, tooltip="Enable dependency parsing"),
-        
+
         compute=widgets.Button(description='Compute', button_style='Success', layout=lw('100px'))
     )
-    
+
     display(widgets.VBox([
         gui.progress,
         widgets.HBox([
@@ -159,7 +159,7 @@ def display_corpus_load_gui(data_folder, document_index=None, container=None, co
         if max_step is not None:
             gui.progress.max = max_step
         gui.progress.value = gui.progress.value + 1 if step is None else step
-            
+
     def compute_callback(*_args):
         try:
             gui.output.clear_output()
@@ -187,6 +187,6 @@ def display_corpus_load_gui(data_folder, document_index=None, container=None, co
             throw
         finally:
             gui.compute.disabled = False
-            
+
     gui.compute.on_click(compute_callback)
 
