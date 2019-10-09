c1cb2970270ab80e6f21ae37bafc7dcf661b76be
roger-mahler <roger.mahler@hotmail.com>
2019-10-09 16:46:13 +0200

    Refactor: Moved utilities to separate folder

---
+++ text_analytic_tools/utility/__init__.py
@@ -0,0 +1,7 @@
+from . utility import *
+from . color_utility import *
+from . config import *
+from . file_utility import *
+from . wordcloud import *
+import text_analytic_tools.utility.widgets as widgets
+
