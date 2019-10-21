a9b4c3cb00f2500f363cb5646124c18c8db37ddd
roger-mahler <roger.mahler@hotmail.com>
2019-10-21 14:51:52 +0200

    Added FileTextReader

--- text_analytic_tools/utility/file_utility.py
+++ text_analytic_tools/utility/file_utility.py
@@ -1,13 +1,61 @@
 
-import shutil
-import zipfile
-import time
 import os
+import re
+import shutil
+import time
+import zipfile
+import typing
+import types
+import fnmatch
 import pandas as pd
-
-from . utils import getLogger
+import gensim
+from .utils import getLogger
 
 logger = getLogger()
+
+HYPHEN_REGEXP = re.compile(r'\b(\w+)-\s*\r?\n\s*(\w+)\b', re.UNICODE)
+
+def dehyphen(text: str):
+    result = re.sub(HYPHEN_REGEXP, r"\1\2\n", text)
+    return result
+
+def list_files(path_name, pattern):
+    px = lambda x: pattern.match(x) if isinstance(pattern, typing.re.Pattern) else fnmatch.fnmatch(x, pattern)
+    if os.path.isdir(path_name):
+        files = [ f for f in os.listdir(path_name) if os.path.isfile(os.path.join(path_name, f)) ]
+    else:
+        with zipfile.ZipFile(path_name) as zf:
+            files = zf.namelist()
+
+    return [ name for name in files if px(name) ]
+
+def compress_whitespaces(text):
+    result = re.sub(r'\s+', ' ', text).strip()
+    return result
+
+def extract_metadata(filename, **kwargs):
+    params = { x: None for x in kwargs.keys()}
+    meta =  types.SimpleNamespace(filename=filename, **params)
+    for k,r in kwargs.items():
+        if r is None:
+            continue
+        if isinstance(r, str): # typing.re.Pattern):
+            m = re.match(r, filename)
+            if m is not None:
+                v = m.groups()[0]
+                meta.__setattr__(k, int(v) if v.isnumeric() else v)
+    return meta
+
+def read_file(path, filename):
+    if os.path.isdir(path):
+        with open(os.path.join(path, filename), 'r') as file:
+            content = file.read()
+    else:
+        with zipfile.ZipFile(path) as zf:
+            with zf.open(filename, 'r') as file:
+                content = file.read()
+    content = gensim.utils.to_unicode(content, 'utf8', errors='ignore')
+    return content
 
 class FileUtility:
 
