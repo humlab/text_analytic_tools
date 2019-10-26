558f5bac9e9b0946326e69a74a8d1c137f1da0b5
roger-mahler <roger.mahler@hotmail.com>
2019-10-26 23:11:45 +0200

    Added exports

--- text_analytic_tools/__init__.py
+++ text_analytic_tools/__init__.py
@@ -4,4 +4,3 @@ from . utility import utils
 from . utility import widgets
 from . utility.config import *
 from . utility import color_utility
-from . domain_logic_config import current_domain as domain_logic
